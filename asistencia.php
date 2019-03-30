db546ba1b4cfcaa0c6f861ff9a6eabcbba5430d1
AndresReyesTech <andres@reyes.tech>
2019-03-30 09:01:27 -0300

    Responsive

--- asistencia.php
+++ asistencia.php
@@ -4,6 +4,8 @@
   <head>
     <meta charset="UTF-8">
     <title>Asistencia — Registro de Asistencia DuocUC</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   </head>
 
@@ -603,6 +605,7 @@
           </tr>
         </tbody>
       </table>
+      <a hef=""
       </div>
     </div>
 
@@ -619,8 +622,26 @@
                   ...
                 </div>
                 <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary">Save changes</button>
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="modal fade" id="exampleSaveSuccess" tabindex="-1" role="dialog" aria-labelledby="exampleSaveSuccess" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalCenterTitle">Confirmación</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
                 </div>
               </div>
             </div>
--- index.php
+++ index.php
@@ -4,6 +4,8 @@
   <head>
     <meta charset="UTF-8">
     <title>Inicio de Sesión — Registro de Asistencia DuocUC</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="/assets/css/login.css"/>
   </head>
